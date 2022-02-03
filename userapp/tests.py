332bafeb62cf983a600aaaf3f5a9273cc6fc58eb
Sergey <segairk@gmail.com>
2022-02-03 22:01:19 +0800

    Lesson 8. Done.

--- userapp/tests.py
+++ userapp/tests.py
@@ -1,3 +1,17 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import UserModelViewSet
+from .models import CustomUser
 
-# Create your tests here.
+
+class TestUserViewSet(TestCase):
+
+    def test_get_detail(self):
+        user = mixer.blend(CustomUser)
+        client = APIClient()
+        response = client.get(f'/api/users/{user.uuid}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
