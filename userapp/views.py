b14fee545765c6ae296e5382aa26896d25c7cd8d
Sergey <segairk@gmail.com>
2022-02-04 18:45:23 +0800

    Lesson 9. Version and Documentation

--- userapp/views.py
+++ userapp/views.py
@@ -1,8 +1,13 @@
 from rest_framework import mixins, viewsets
 from .models import CustomUser
-from .serializers import UserModelSerializer
+from .serializers import UserModelSerializer, UserModelSerializerNew
 
 
 class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
     serializer_class = UserModelSerializer
     queryset = CustomUser.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.version == '0.2':
+            return UserModelSerializerNew
+        return UserModelSerializer
